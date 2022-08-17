e93b170a6e163c3e534dcc957010eec5d4e6d43a
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2022-08-17 23:43:22 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '1fgvsqlite',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
